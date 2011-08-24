ccab886f29b4422b23bc6615e5ce65a548ed2c94
Pony Smith <pony@sfsu.edu>
2011-08-24 10:31:48 -0700

    Adding template files for Test user creation and User assignment

---
+++ app/views/system/assign.tpl.php
@@ -0,0 +1,34 @@
+<h1>Assign User to Course</h1>
+
+<form action="{$smarty.const.CURRENT_URL}" method="post">
+	<input type="hidden" name="submit_token" value="{$smarty.const.SUBMIT_TOKEN}" />
+    
+    <div class="message info">
+    Use this form to assign a user a specific role in a specific course
+	</div>
+    
+    <div class="label"><label for="enroll_user_id">User Id</label></div>
+    <div class="input"><input type="text" name="enroll_user_id" id="enroll_user_id" style="width: 150px;" value="{$smarty.const.TEST_IDP_USER}" /></div>
+    <div style="clear: both;"></div>
+	
+    <div class="label"><label for="enroll_class_id">Course Id</label></div>
+    <div class="input"><input type="text" name="enroll_class_id" id="enroll_class_id" style="width: 150px;" /><span class="form_note" style="margin-left: 10px;">Use course SSID (eg. 20114-R-12345)</span></div>
+    <div style="clear: both;"></div>
+	
+    <div class="label"><label for="enroll_role">Insert As</label></div>
+    <div class="input">
+		<select name="enroll_role" id="enroll_role" style="width: 160px;">
+			<option value="instructor">Instructor</option>
+			<option	value="student">Student</option>
+		</select>
+	</div>
+    <div style="clear: both;"></div>
+
+    <div class="save_row">
+		<div class="label">&nbsp;</div>
+		<div class="input">
+			<input type="submit" name="command[assignUser]" value="Assign" class="button" />
+		</div>			
+    </div>
+    
+</form>
---
+++ app/views/system/create_users.tpl.php
@@ -0,0 +1,32 @@
+<h1>Create Admin User for Testing</h1>
+
+<form action="{$smarty.const.CURRENT_URL}" method="post">
+	<input type="hidden" name="submit_token" value="{$smarty.const.SUBMIT_TOKEN}" />
+    
+    <div class="message warn">
+    <p>
+    This will create a set of users for testing purposes. The users should be able to log in via Shibboleth on the SFSU Test IDP.
+    </p>
+		<h3>Admin User</h3>
+		<ul>
+			<li>T10000001</li>
+		</ul>
+		
+		<h3>Normal Users</h3>
+		<ul>
+			<li>T20000001</li>
+			<li>T30000001</li>
+			<li>T40000001</li>
+			<li>T50000001</li>
+			<li>T60000001</li>
+		</ul>
+	
+    
+    <p>Are you sure you want to continue?</p>
+
+    <div class="save_row">
+        <input type="submit" name="command[createUsers]" value="Create Users" class="button" />
+    </div>
+	</div>
+    
+</form>
