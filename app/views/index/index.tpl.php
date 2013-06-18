3e832a70cb29eafa97625255850c19dbf2d4f842
Charles O'Sullivan <chsoney@sfsu.edu>
2013-06-18 11:34:26 -0700

    Changed the test on front page.

--- app/views/index/index.tpl.php
+++ app/views/index/index.tpl.php
@@ -1,8 +1,11 @@
     <h1>Online Syllabus Tool</h1>
-    <p>
-    Academic Technology's Syllabus tool allows faculty and academic office coordinators to build and manage class syllabi. This service draws content from campus databases and allows users to create, share, and re-use syllabi in ways that save time and allow for a paperless approach to managing this resource.  Users can publish accessible versions in multiple formats including directly into iLearn courses.
-    </p>
-    <p>
-    If you are a faculty member, you may begin building your online syllabi by <a href="syllabus">Logging in</a>. If you have questions
-    about <em>Syllabus</em>, you please <a href="contact">contact the <em>Syllabus</em> team</a>.
-    </p>
+
+    <p>Academic Technology's Syllabus tool allows faculty and academic office coordinators to build and manage class syllabi. The service draws content from campus databases and allows users to create, share, and re-use syllabi in ways that save time and allow for a paperless approach to managing this resource. Users can publish accessible versions in multiple formats including directly into iLearn courses.</p>
+    
+    <p>Faculty can start using this service by simply <a href="syllabus">logging in</a>.</p>
+     
+    <p>If you have questions or need help, please contact our AT Help Desk</p>
+    <ul>
+    <li>Phone: 415-405-5555</li>
+    <li>Email: <a href="mailto:online@sfsu.edu">online@sfsu.edu</a></li>
+    </ul>
