e66e2015d84d244d699a5b8548382885ab57e72e
Charles O'Sullivan <chsoney@sfsu.edu>
2014-08-05 09:37:35 -0700

    Did not save update script sql.

---
+++ app/scripts/update-2014-08-04.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE semester_info (
+    id  int(11) NOT NULL,
+    activity   tinyint(1) DEFAULT 0,
+    visibility tinyint(1) DEFAULT 0
+);
+
+ALTER TABLE syllabus ADD COLUMN syllabus_sem_id int(11);
+
+UPDATE syllabus SET syllabus_sem_id = (syllabus_class_year * 10) + syllabus_class_semester;
