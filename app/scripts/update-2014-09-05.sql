0011a180929773bd30b6a65101bff7b3b207d5a5
Saru Mehta <sarmehta88@gmail.com>
2014-09-10 13:30:36 -0700

    Added indices to run the deletions from enrollment faster

--- app/scripts/update-2014-09-05.sql
+++
@@ -1,3 +0,0 @@
-create index ID on syllabus (syllabus_id);
-create index SID on syllabus (syllabus_sem_id);
-create index DID on enrollment (enroll_class_id);
---
+++ app/scripts/update-2014-09-10.sql
@@ -0,0 +1,2 @@
+create index SID on syllabus (syllabus_sem_id);
+create index ER on enrollment (enroll_role);
