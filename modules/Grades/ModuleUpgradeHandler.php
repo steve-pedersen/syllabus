b7b9d864ec2231832dce49c45f1d12ca2a87992a
Steve Pedersen <spedersen@spedersen18.dev.at.sfsu.edu>
2019-07-17 16:33:04 -0700

    Default Grades section table matches the one in iLearn.

--- modules/Grades/ModuleUpgradeHandler.php
+++ modules/Grades/ModuleUpgradeHandler.php
@@ -83,6 +83,100 @@ class Syllabus_Grades_ModuleUpgradeHandler extends Bss_ActiveRecord_BaseModuleUp
                 ], ['idList' => ['id']]);
                
                break;
+
+            case 1:
+
+                $this->useDataSource('Syllabus_Grades_Grades');
+                $defaultGradesMap = $this->insertRecords('syllabus_grades', [
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'columns' => 2,
+                        'header1' => 'Percentage Range',
+                        'header2' => 'Letter Grade'
+                    ]
+                ], ['idList' => ['id']]);
+                $gradesId = $defaultGradesMap[0]['id'];
+
+                $siteSettings->defineProperty('sections-grades-default-id', 'Grades section defaults', 'int');
+                $siteSettings->setProperty('sections-grades-default-id', $gradesId);
+
+                $this->useDataSource('Syllabus_Grades_Grade');
+                $defaultGradeMap = $this->insertRecords('syllabus_grades_grades', [
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'grades_id' => $gradesId,
+                        'column1' => '100.00% - 93.00%',
+                        'column2' => 'A',
+                    ],
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'grades_id' => $gradesId,
+                        'column1' => '92.99% - 90.00%',
+                        'column2' => 'A-',
+                    ],
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'grades_id' => $gradesId,
+                        'column1' => '89.99% - 87.00%',
+                        'column2' => 'B+',
+                    ],
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'grades_id' => $gradesId,
+                        'column1' => '86.99% - 83.00%',
+                        'column2' => 'B',
+                    ],
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'grades_id' => $gradesId,
+                        'column1' => '82.99% - 80.00%',
+                        'column2' => 'B-',
+                    ],
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'grades_id' => $gradesId,
+                        'column1' => '79.99% - 77.0%',
+                        'column2' => 'C+',
+                    ],
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'grades_id' => $gradesId,
+                        'column1' => '76.99% - 73.00%',
+                        'column2' => 'C',
+                    ],
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'grades_id' => $gradesId,
+                        'column1' => '72.99% - 70.00%',
+                        'column2' => 'C-',
+                    ],
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'grades_id' => $gradesId,
+                        'column1' => '69.99% - 67.00%',
+                        'column2' => 'D+',
+                    ],
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'grades_id' => $gradesId,
+                        'column1' => '66.99% - 63.00%',
+                        'column2' => 'D',
+                    ],
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'grades_id' => $gradesId,
+                        'column1' => '62.99% - 60.00%',
+                        'column2' => 'D-',
+                    ],
+                    [
+                        'id' => ['type' => 'int', 'sequence' => true],
+                        'grades_id' => $gradesId,
+                        'column1' => '59.99% - 0.00%',
+                        'column2' => 'F',
+                    ]
+                ], ['idList' => ['id']]);
+               
+               break;
         }
     }
 }
