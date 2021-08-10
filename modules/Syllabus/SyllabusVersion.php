b62f75cb4e60e38e034222e12f659458bb2beb37
Steve Pedersen <spedersen@spedersen18.dev.at.sfsu.edu>
2021-08-10 11:43:43 -0700

    undo temp fix

--- modules/Syllabus/SyllabusVersion.php
+++ modules/Syllabus/SyllabusVersion.php
@@ -83,13 +83,9 @@ class Syllabus_Syllabus_SyllabusVersion extends Bss_ActiveRecord_Base
                 $sv->log           = $this->sectionVersions->getProperty($oldSv, 'log');
 
                 $sv->normalizedVersion = $sv->section->getNormalizedVersion($sv->id);
-
-                $order = str_pad($sv->sortOrder, 3, "0", STR_PAD_LEFT);
-
-                $sectionVersions[$order] = $sv;
+                $sectionVersions[] = $sv;
             }
         }
-        ksort($sectionVersions, SORT_NATURAL);
 
         return $sectionVersions;
     }
